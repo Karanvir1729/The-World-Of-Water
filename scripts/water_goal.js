1868d255f2bd779990475b137eb774e98a471df3
M H <772623@pdsb.net>
2020-10-26 05:08:45 +0000

    Water Goal Page

--- scripts/water_goal.js
+++ scripts/water_goal.js
@@ -1,21 +1,6 @@
 var todaysDate = new Date()
 var month = todaysDate.getMonth()
-/*
-const header = document.getElementById("head")
-const goalHeader = document.createElement("h2")
-goalHeader.id = "header"
 
-header.style.width = window.innerWidth * 0.75 + "px"
-header.style.left = (window.innerWidth / 2) - (window.innerWidth / 2.75) + "px"
-
-const buttons = document.getElementById("buttons")
-const manuallyEnter = document.getElementById("manual")
-const activity = document.getElementById("activity")
-
-
-manuallyEnter.style.left = (window.innerWidth / 2) - 220 + "px"
-activity.style.left = (window.innerWidth / 2) + 10 + "px"
-*/
 function random(start, end){
   end -= start
   return Math.round(Math.random() * end + start)
@@ -25,12 +10,6 @@ function updateWaterUsage(usage) {
   var waterRemaining = dailyGoal - usage
   return waterRemaining
 }
-/*
-window.addEventListener("load", () => {
-  document.getElementById("banner").width = window.innerWidth
-  document.getElementById("banner").height = window.innerHeight / 3
-})
-*/
 
 
 //Calculates the water goal in Liters based on the months
@@ -43,31 +22,7 @@ if (0 <= month && month <= 2){
   var dailyGoal = random(225, 275)
 }
 
-/*
-goalHeader.innerHTML = "Today's Goal: " + dailyGoal + "L"
-header.appendChild(goalHeader)
 
-manuallyEnter.addEventListener("click", () => {
-  
-  waterUsage.placeholder = "Enter in water usage"
-  
-
-  if (inputs.childElementCount < 1){
-    inputs.appendChild(waterUsage)
-    inputs.appendChild(submit)
-  }
-})
-
-
-submit.addEventListener("click", () => {
-  dailyGoal = updateWaterUsage(waterUsage.value)
-  console.log(dailyGoal)
-})
-
-document.addEventListener("click", event => {
-  console.log(event.pageX, event.pageY)
-})
-*/
 const leftOption = document.getElementById("leftoption")
 const rightOption = document.getElementById("rightoption")
 
@@ -77,10 +32,17 @@ rightOption.style.height = window.innerHeight - 180 + "px"
 leftOption.children[0].style.padding = (window.innerHeight - 180) / 2 - 72 + "px"
 rightOption.children[0].style.padding = (window.innerHeight - 180) / 2 - 72 + "px"
 
+const background = document.getElementById("background")
+  background.style.width = window.innerWidth
+  background.style.height = window.innerHeight - 70 + "px"
+
+const header = document.createElement("h1")
+header.innerHTML = "Enter Water Usage"
+header.id = "header"
+
 const inputs = document.getElementById("inputs")
-  inputs.style.width = "560px"
+  inputs.style.width = "555px"
   inputs.style.margin = "auto"
-  inputs.style.border = "solid 1px black"
   inputs.style.position = "relative"
   inputs.style.top = window.innerHeight / 3 + "px"
 
@@ -97,9 +59,10 @@ leftOption.addEventListener("click", () => {
   leftOption.classList.add("wipeRight")
   setTimeout(() => {
     leftOption.remove()
+    background.appendChild(header)
     inputs.appendChild(waterUsage)
     inputs.appendChild(submit)
-    
+    goalHeader.remove()
     }, 1000)
 })
 
@@ -107,4 +70,35 @@ rightOption.addEventListener("click", () => {
   leftOption.remove()
   rightOption.classList.add("wipeLeft")
   setTimeout(() => {rightOption.remove()}, 1000)
+})
+
+const container = document.getElementById("container")
+const goalHeader = document.createElement("h2")
+goalHeader.style.position = "absolute"
+goalHeader.style.top = window.innerHeight / 5 + "px"
+goalHeader.style.left = window.innerWidth / 2 - 140 + 'px'
+goalHeader.style.fontSize = "36px"
+goalHeader.innerHTML = "Water Goal " + dailyGoal + "L"
+goalHeader.classList.add("center")
+
+container.appendChild(goalHeader)
+
+submit.addEventListener("click", () => {
+  if (waterUsage.value < 0 || isNaN(waterUsage.value)){
+    const warning = document.createElement("p")
+    warning.innerHTML = "Enter postive numbers only"
+    warning.style.color = "red"
+    warning.style.position = "relative"
+    warning.style.top = "50px"
+    warning.style.display = "block"
+    warning.style.margin = "auto"
+    if (container.childElementCount < 4){
+      container.appendChild(warning)
+    }
+    return 
+  }
+  if (confirm("Are you sure that you used " + waterUsage.value + "L of water.\nOnce this is entered it cannot be changed")){
+    dailyGoal = dailyGoal - waterUsage.value
+  }
+  console.log(dailyGoal)
 })
